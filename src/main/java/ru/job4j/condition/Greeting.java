f73f87705bc7046110ce72959e44884af78de776
diac <nikolai.gladkikh@gmail.com>
2022-08-03 08:22:43 +0300

    Исправлена ошибка стиля "Убирайте пустые строки внутри метода" [#237407 #328118]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -6,13 +6,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-
         idea += " But I am a newbie";
-
         final String year = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-
         idea += " " + year;
-
         System.out.println(idea);
     }
 }
