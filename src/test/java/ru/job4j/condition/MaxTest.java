f94e39ae67e03fd4ccd4e80f2221be7b8eb4cbd2
diac <nikolai.gladkikh@gmail.com>
2022-08-04 14:53:32 +0300

    Добавить еще не менее двух тестов: 4.2. Тернарное сравнение [#189 #328567]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -13,4 +13,31 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax10ToMax999Then999() {
+        int left = 10;
+        int right = 999;
+        int result = Max.max(left, right);
+        int expected = 999;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax100To1Then100() {
+        int left = 100;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax33To33Then33() {
+        int left = 33;
+        int right = 33;
+        int result = Max.max(left, right);
+        int expected = 33;
+        assertThat(result).isEqualTo(expected);
+    }
 }
