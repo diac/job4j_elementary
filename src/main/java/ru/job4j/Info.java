f263d008a32a4daee1c4931a95f4e1b6ac9a7ebf
diac <nikolai.gladkikh@gmail.com>
2022-08-04 10:47:08 +0300

    Добавить javadoc к методу main: 4.2. Заголовок в файле java [#159036 #327713]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,6 +1,11 @@
 package ru.job4j;
 
 public class Info {
+    
+    /**
+     * Задание 4.2. Заголовок в файле java
+     * @param args Параметры запуска метода
+     */
     public static void main(String[] args) {
         System.out.println("01.08.2022");
     }
