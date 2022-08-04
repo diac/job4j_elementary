1e36faa446d231d67796c4f7f5eb5d5a774fa6ef
diac <nikolai.gladkikh@gmail.com>
2022-08-05 00:42:23 +0300

    Исправить стиль кода: 5.5. Простое число [#171694 #328964]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number > 1;
 
-        if (number == 1) {
-            return false;
-        }
-
-        for (int d = 2; d < number; d++) {
-            if (number % d == 0) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
                 prime = false;
                 break;
             }
