eaad457f4596105df294d99706db7508df407702
diac <nikolai.gladkikh@gmail.com>
2022-08-05 09:34:27 +0300

    Убрать guard clause, исправить ошибки стиля: 6.4.3. Слово заканчивается на ... [#156317 #329047]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,17 +3,12 @@ package ru.job4j.array;
 public class EndsWith {
 
     public static boolean endsWith(char[] word, char[] post) {
-        if (post.length > word.length) {
-            return false;
-        }
-
-        for (int i = post.length - 1; i >= 0; i--) {
+        for (int i = 0; i < post.length; i++) {
             int j = word.length + i - post.length;
             if (word[j] != post[i]) {
                 return false;
             }
         }
-
         return true;
     }
 }
