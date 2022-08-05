ce5657d045b0a2b0db52f2f07be48e181f92b2b3
diac <nikolai.gladkikh@gmail.com>
2022-08-05 09:26:20 +0300

    Избавиться от лишней переменной: 6.2.3. Перевернуть массив. [#226790 #329015]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -4,10 +4,9 @@ public class Turn {
 
     public static int[] back(int[] array) {
         for (int i = 0; i < array.length / 2; i++) {
-            int srcVal = array[i];
-            int dstVal = array[array.length - i - 1];
-            array[i] = dstVal;
-            array[array.length - i - 1] = srcVal;
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
         return array;
     }
