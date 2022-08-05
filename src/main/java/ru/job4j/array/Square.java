431d8e6fcde8b978f4d2dfc314f4775fad1ffbdc
diac <nikolai.gladkikh@gmail.com>
2022-08-05 08:15:56 +0300

    Исправить ошибки стиля: 6.0.6. Заполнить массив степенями чисел. [#33488 #328997]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -4,11 +4,9 @@ public class Square {
 
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
-
         for (int i = 0; i < bound; i++) {
             rst[i] = i * i;
         }
-
         return rst;
     }
 
