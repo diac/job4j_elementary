0b2209fa2b4524a47a57c9408e344709e794a1fa
diac <nikolai.gladkikh@gmail.com>
2022-08-05 12:28:50 +0300

    Оптимизировать алгоритм: 6.7.4. Выигрышные комбинации в сокобан [#53859 #329181]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -35,6 +35,9 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
+            if (board[i][i] != 'X') {
+                continue;
+            }
             if ((monoHorizontal(board, i) || monoVertical(board, i))) {
                 result = true;
                 break;
